21f6caa39a535d4e1ae5415eb15defc7735e5a54
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-10-12 15:44:06 -0700

    Finished the assignment

---
+++ index.ts
@@ -0,0 +1,37 @@
+let myNum: number = 5;
+let myString: string = "Hello Universe";
+let myArr: Array < number > = [1, 2, 3, 4];
+let myObj = {
+    name: "Bill"
+};
+let anythinVariable: any = "Hey";
+anythinVariable = 25;
+let arrayOne: Array < boolean > = [true, false, true, true];
+let arrayTwo: Array < any > = [1, 'abc', true, 2];
+let myObj1 = {
+    x: 5,
+    y: 10
+};
+class MyNode {
+    val: number;
+    _priv: number;
+
+    constructor(val: number) {
+        this.val = 0;
+        this.val = val;
+    }
+    doSomething() {
+        this._priv = 10;
+    }
+    return MyNode;
+}
+var myNodeInstance = new MyNode(1);
+
+function myFunction(): void {
+    console.log("Hello World");
+    return;
+}
+
+function sendingErrors(): never {
+    throw new Error('Error message');
+}
